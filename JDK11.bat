9bbdc4ced681f620fb275a551fb6d9ffa1d0441b
root046 <64629095+root046@users.noreply.github.com>
2023-06-13 15:03:24 +0300

    Add files via upload

---
+++ JDK11.bat
@@ -0,0 +1,4 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.16.1
+set Path=%JAVA_HOME%\bin;%Path%
+echo Java JDK 11 activated.
---
+++ JDK19.bat
@@ -0,0 +1,4 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk-19
+set Path=%JAVA_HOME%\bin;%Path%
+echo Java JDK 19 activated.
